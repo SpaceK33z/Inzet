6484fdff6fee021973a5af1b28d0cd59bb2c0c0c
Kees Kluskens <kees@webduck.nl>
2014-12-08 16:11:20 +0100

    Terms in json

--- app/js/controllers/main.js
+++ app/js/controllers/main.js
@@ -1,8 +1,12 @@
-appCtrls.controller('MainCtrl', function($scope, userService, $location, $route, $routeParams) {
+appCtrls.controller('MainCtrl', function($scope, userService, $location, $route, $routeParams, $http) {
   $scope.username = userService.getName() || 'Bart';
 
   // Change the selected sidebar when the route parameters change.
   $scope.$on('$routeChangeSuccess', function (ev, current, prev) {
     $scope.selectedSidebar = $routeParams.term || false;
+  });
+
+  $http.get('/test-terms.json').success(function(data) {
+    $scope.terms = data;
   });
 });
